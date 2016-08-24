c6ffe2df677d40d71f99b3627a807dfc070c2ae4
Mannaio <info@tommasovilla.com>
2016-08-24 23:36:35 +0200

    Fixtures For new SVG Graphic

--- build.js
+++ build.js
@@ -2,8 +2,8 @@ import fs from 'fs';
 import React from 'react';
 // import { renderToStaticMarkup } from 'react-dom/server';
 import Manna from './Manna';
-import Cog from './Cog';
-import Square from './Square';
+// import Cog from './Cog';
+// import Square from './Square';
 import Polygon from './Polygon';
 import Line from './Line';
 
@@ -13,9 +13,9 @@ const build = (name="defaultName", props)=> {
     var n = 1;
 
     for (var i = 0; i < n; i++) {
-      svgs.push(Cog.prototype.render());
+      // svgs.push(Cog.prototype.render());
       svgs.push(Manna.prototype.render());
-      svgs.push(Square.prototype.render());
+      // svgs.push(Square.prototype.render());
       svgs.push(Polygon.prototype.render());
       svgs.push(Line.prototype.render());
     }
--- src/manna.jsx
+++ src/manna.jsx
@@ -17,13 +17,17 @@ const Manna = React.createClass({displayName: "Cog",
   initVal: {
     likes: 10,
     code: "2",
-    size: "64",
+    size: 350,
     d1: 1,
     d2: .6875,
     d3: .375,
-    teeth: 4,
+    d4: 1.5217,
+    d5: 3.5,
+    d6: 1.94,
+    d7: 2.12,
+    teeth: 6,
     splay: 0.375,
-    fill: 'currentcolor',
+    fill: 'c60000',
     caption: 'Third SVG'
   },
 
@@ -32,8 +36,7 @@ const Manna = React.createClass({displayName: "Cog",
     const caption = this.initVal.caption
     const likes = this.initVal.likes
     const code = this.initVal.code
-    // const size = this.initVal.size
-    const size = Math.random()*100
+    const size = this.initVal.size
     const fill = this.initVal.fill
 
     // Center
@@ -43,6 +46,19 @@ const Manna = React.createClass({displayName: "Cog",
     const r1 = this.initVal.d1 * size / 2
     const r2 = this.initVal.d2 * size / 2
     const r3 = this.initVal.d3 * size / 2
+    const r4 = this.initVal.d4
+    const r5 = this.initVal.d5
+    const r6 = this.initVal.d6
+    const r7 = this.initVal.d7
+
+    // Circle x coordinates
+    const cx1 = size / r4
+    const cx2 = size / r5
+    const cx3 = size / r6
+    const cx4 = size / r7
+
+    // Circle y coordinates
+
 
     // Angle
     const angle = 360 / this.initVal.teeth
@@ -50,13 +66,37 @@ const Manna = React.createClass({displayName: "Cog",
 
     const viewBox = [0, 0, size, size].join(' ')
 
+    const rad = function(a) {
+      return Math.PI * a / 180
+    }
+
+    const rx = function(r, a) {
+      return c + r * Math.cos(rad(a))
+    }
+
+    const ry = function(r, a) {
+      return c + r * Math.sin(rad(a))
+    }
+    const num = function(n) {
+      return (n < 0.0000001) ? 0 : n
+    }
+
+    const drawTeeth = function(n) {
+      const d = []
+      for (var i = 0; i < n; i++) {
+        const a = angle * i - offset
+        const line = [
+          (i === 0) ? 'M' : 'L',
+          num(rx(r1, a)),
+          num(ry(r1, a)),
+        ].join(' ')
+        d.push(line)
+      }
+      return d.join(' ')
+    }
 
     const pathData = [
-      'M', 2, 2, // Move to 2,2
-      'L', 62, 2, // Draw a line to 62,2
-      'L', 62, 62, // Draw a line to 62,62
-      'L', 2, 62, // Draw a line to 2,62
-      'L', 2, 2, // Draw a line to 2,2
+      drawTeeth(this.initVal.teeth)
     ].join(' ')
 
     return {
@@ -68,6 +108,7 @@ const Manna = React.createClass({displayName: "Cog",
       h: size,
       fill: fill,
       d: pathData,
+      cx: cx1
     }
 
   }
