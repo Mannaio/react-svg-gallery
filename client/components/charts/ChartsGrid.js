2fb90bfbe3dc80d210c4d2c0c872caccad410501
Mannaio <info@tommasovilla.com>
2017-01-08 15:09:07 +0100

    Merge Conflict Fixed

--- client/components/charts/ChartsGrid.js
+++ client/components/charts/ChartsGrid.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Chart from './Chart';
+import LineChart from './d3/LineChart';
 
 export default class ChartsGrid extends React.Component{
 
   render() {
+
     const view = [480, 320];
     const trbl = [0, 0, 0, 0]
     const horizontalAxisHeight = 30;
@@ -18,26 +20,3 @@ export default class ChartsGrid extends React.Component{
     )
   }
 };
-
-// import React from 'react';
-// import Chart from './Chart';
-// import LineChart from './d3/LineChart';
-//
-// export default class ChartsGrid extends React.Component{
-//
-//   render() {
-//
-//     const view = [480, 320];
-//     const trbl = [0, 0, 0, 0]
-//     const horizontalAxisHeight = 30;
-//     const verticalAxisWidth = 42;
-//     const nums  = this.props.nums || [];
-//     // const nums = [32, 19, 47, 38, 17, 62, 94, 21, 59, 62];
-//
-//     return (
-//       <div className="grid">
-//           <LineChart {...{view, trbl, nums, horizontalAxisHeight, verticalAxisWidth}} />
-//       </div>
-//     )
-//   }
-// };
